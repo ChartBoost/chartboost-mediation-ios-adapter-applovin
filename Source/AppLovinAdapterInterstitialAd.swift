0e61f2dcfba00c93682a1925b2bc18acab0d7f79
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 10:57:22 +0530

    Update banners conformance

--- Source/AppLovinAdapterInterstitialAd.swift
+++ Source/AppLovinAdapterInterstitialAd.swift
@@ -9,16 +9,7 @@ import Foundation
 import UIKit
 
 /// The Chartboost Mediation AppLovin adapter interstitial ad.
-class AppLovinAdapterInterstitialAd: AppLovinAdapterAd, PartnerAd {
-    
-    /// The partner ad view to display inline. E.g. a banner view.
-    /// Should be nil for full-screen ads.
-    var inlineView: UIView? { nil }
-    
-    /// The loaded partner ad banner size.
-    /// Should be `nil` for full-screen ads.
-    var bannerSize: PartnerBannerSize? { nil }
-
+class AppLovinAdapterInterstitialAd: AppLovinAdapterAd, PartnerFullscreenAd {
     /// The AppLovin loaded ad instance.
     var ad: ALAd?
     
