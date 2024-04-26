3827b75ecd31e5c2bcd9c1a280f590b904e3b527
Daniel Barros <daniel.barros@chartboost.com>
2024-04-26 12:24:54 -0700

    HB-7647: Configuration class rework (#70)
    * Relocate adapter properties to configuration
    
    * Expand configuration options
    
    * Add public keyword
    
    * Use actions v2 with support for relocated partnerVersion definition

--- Source/AppLovinAdapterConfiguration.swift
+++ Source/AppLovinAdapterConfiguration.swift
@@ -10,12 +10,36 @@ import os.log
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class AppLovinAdapterConfiguration: NSObject {
     
+    /// The version of the partner SDK.
+    @objc public static var partnerSDKVersion: String {
+        ALSdk.version()
+    }
+
+    /// The version of the adapter.
+    /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
+    /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
+    @objc public static let adapterVersion = "4.12.4.0.0"
+
+    /// The partner's unique identifier.
+    @objc public static let partnerID = "applovin"
+
+    /// The human-friendly partner name.
+    @objc public static let partnerDisplayName = "AppLovin"
+
     private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.applovin", category: "Configuration")
 
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
     @objc public static var testMode: Bool = false
-    
+
+    /// Flag that can optionally be set to disable the partner's audio.
+    /// Disabled by default.
+    @objc public static var isMuted: Bool = false {
+        didSet {
+            syncIsMuted()
+        }
+    }
+
     /// Flag that can optionally be set to enable the partner's verbose logging.
     /// Disabled by default.
     @objc public static var verboseLogging: Bool = false {
@@ -37,24 +61,26 @@ import os.log
 extension AppLovinAdapterConfiguration {
     
     /// The AppLovin SDK instance
-    static let sdk: ALSdk = ALSdk.shared()
+    private static let sdk: ALSdk = ALSdk.shared()
 
     static func sync() {
         syncVerboseLogging()
         syncLocationCollection()
+        syncIsMuted()
     }
 
-    static func syncVerboseLogging() {
+    private static func syncVerboseLogging() {
         sdk.settings.isVerboseLoggingEnabled = verboseLogging
-        if #available(iOS 12.0, *) {
-            os_log(.debug, log: log, "AppLovin SDK verbose logging set to %{public}s", "\(verboseLogging)")
-        }
+        os_log(.debug, log: log, "AppLovin SDK verbose logging set to %{public}s", "\(verboseLogging)")
     }
 
-    static func syncLocationCollection() {
+    private static func syncLocationCollection() {
         sdk.settings.isLocationCollectionEnabled = locationCollection
-        if #available(iOS 12.0, *) {
-            os_log(.debug, log: log, "AppLovin SDK location collection set to %{public}s", "\(locationCollection)")
-        }
+        os_log(.debug, log: log, "AppLovin SDK location collection set to %{public}s", "\(locationCollection)")
+    }
+
+    private static func syncIsMuted() {
+        sdk.settings.isMuted = isMuted
+        os_log(.debug, log: log, "AppLovin SDK isMuted set to %{public}s", "\(isMuted)")
     }
 }
